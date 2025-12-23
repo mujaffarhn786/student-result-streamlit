2c461b8a5cd704b860f283fdcca903a80f8f21a4
mujaffar husain <mujaffarhn786@gmail.com>
2025-12-23 10:39:18 +0530

    Update model.py

--- model.py
+++ model.py
@@ -2,18 +2,20 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 import pickle
 
-# Load dataset
+# Load clean CSV
 data = pd.read_csv("student_data.csv")
+
+print("Columns:", data.columns)
 
 X = data[['StudyHours', 'Attendance', 'PreviousMarks']]
 y = data['Result']
 
-# Train model
 model = LogisticRegression()
 model.fit(X, y)
 
-# Save model
 pickle.dump(model, open("student_model.pkl", "wb"))
+print("Model saved successfully!")
 
-print("Model trained and saved successfully!")
+
+
 
